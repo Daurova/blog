b25eb0dea38e402e0a4cc0921a2c0b1a27cffc62
Daurova <daurova@yandex.ru>
2024-06-21 17:03:24 +0300

    2106 1703

--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -1,50 +1,52 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState, Fragment } from "react"
 import { getArticles } from "../services/services"
-import {List, Avatar} from "antd"
+import {Pagination} from "antd"
 
 const Blog  = () => {
     const [posts, setPosts]=useState([])
-
+    const [information, setInformation]=useState([])
     useEffect(()=>{
-        const getData = async ()=>{
+      const getData = async ()=>{
       const data =  await getArticles()
       console.log (data)
+
       setPosts(data.articles)
     }
     getData()
-    },[]
-    
-    )
-    return (
-        <div>
-            <h1>
-                <p> 
-                    this is a blog with react-router
+    },[])
 
-                </p>
-            </h1>
-            {/* <List
-  itemLayout="horizontal"
-  dataSource={posts}
-  renderItem={(item) => (
-    <List.Item>
-        <p>{item.author.username}</p>
-      <List.Item.Meta
-        avatar={<Avatar src={item.author.image}></Avatar>}
-        title={<a href={item.href}>{item.title}</a>}
-        description={item.description}
-      />
-    </List.Item>
-  )}
-/> */}
-           {posts?.map((item)=><><p>slug:{item.slug}</p>
-           <p><img src = {item.author.image}></img></p>
-           <a href={item.title}>item title: {item.title}</a>
-           <p>item description: {item.description}</p>
-           <p>created at: {item.createdAt}</p>
-           <p>number of likes: {item.favoritesCount}</p>
-           <p>Tags: {item.tagList.map((tag)=><p>{tag}</p>)}</p></>)}
-        </div>
+    useEffect(()=>{
+        const getData = async ()=>{
+        const data =  await getArticles()
+        console.log (data)
+  
+        setInformation(data)
+      }
+      getData()
+      },[]
+    )
+
+
+    return (
+        
+        <div>
+        <h1>
+            <p>this is a blog with react-router</p>
+        </h1>
+        {posts?.map((item, index) => (  
+            <div key={index}> {/* key prop should be on the outermost element */}
+                <p>slug:{item.slug}</p>
+                <p><img src={item.author.image} alt="Author"></img></p>
+                <a href={item.title}>item title: {item.title}</a>
+                <p>item description: {item.description}</p>
+                <p>created at: {item.createdAt}</p>
+                <p>number of likes: {item.favoritesCount}</p>
+                <p>Tags: {item.tagList.map((tag, tagIndex) => <span key={tagIndex}>{tag}</span>)}</p>
+            </div>
+        ))}
+        <Pagination total={information.articlesCount}/>
+    </div>
+
     )
 }
 export default Blog
